6422e10b50a4d6d5505e0f648b56d80db46f4a97
ksh <kangsong77@gmail.com>
2020-08-07 08:35:42 +0900

    0806 ksong

--- src/components/HomeDefault/Banner.js
+++ src/components/HomeDefault/Banner.js
@@ -1,8 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import ModalVideo from 'react-modal-video';
 import '../../../node_modules/react-modal-video/scss/modal-video.scss';
+/*
+const Banner = () => {
+  const [isOpen, setIsOpen] = useState(false);
 
+  const openModal = (e) => {
+    e.preventDefault();
+    setIsOpen(true);
+  };
+
+  return (
+    <div id='home' className='uk-banner uk-dark main-banner item-bg2'>
+      <div className='d-table'>
+        <div className='d-table-cell'>
+          <div className='uk-container'>
+            <div className='main-banner-content'>
+              <h1>
+                Today's Recommended <br /> Movie
+              </h1>
+              <p>
+                We trust on innovation and that help us to succeed in future!
+                Our team is innovative and interested to learn your project to
+                offer the best solutions from out of the box!
+              </p>
+              <Link to='#' className='uk-button uk-button-default'>
+                Get Started
+              </Link>
+
+              <Link
+                onClick={(e) => {
+                  openModal(e);
+                }}
+                to='#'
+                className='video-btn popup-youtube'
+              >
+                <span uk-icon='play'></span> Watch Video
+              </Link>
+
+              <ModalVideo
+                channel='youtube'
+                isOpen={isOpen}
+                videoId='bk7McNUjWgw'
+                onClose={setIsOpen(false)}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+*/
 class Banner extends React.Component {
   state = {
     isOpen: false,
